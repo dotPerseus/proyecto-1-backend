6f6387d2e4ac2b66c80d14e60ca668510ef07e05
Manuel Varela <manuel.imt@hotmail.com>
2026-03-10 15:13:24 -0600

    Refactor: funciones para ver y agregar tareas.

--- tareas.py
+++ tareas.py
@@ -14,6 +14,24 @@ def guardar_tarea(tarea):
     with open("tareas.txt", "a") as archivo:
         archivo.write(tarea + "\n")
 
+def ver_tareas(tareas):
+    if len(tareas) == 0:
+            print("No hay tareas pendientes.")
+    else:
+        print("\nLista de tareas: ")
+        for i, tarea in enumerate(tareas, start=1):
+            print(f"{i}. {tarea}")
+    
+def agregar_tarea(tareas):
+    nueva_tarea = input("Escribe la nueva tarea: ")
+    if nueva_tarea.lower() in [t.lower() for t in tareas]:
+        print ("Esa tarea ya existe.")
+    else:
+        tareas.append(nueva_tarea)
+        guardar_tarea(nueva_tarea)
+        print("Tarea agregada correctamente.")
+    
+
 def mostrar_menu():
     print("\n--- GESTOR DE TAREAS ---")
     print("1. Ver tareas")
@@ -27,20 +45,11 @@ while True:
     opcion = input("Selecciona una opcion: ")
 
     if opcion == "1":
-        if len(tareas) == 0:
-            print("No hay tareas pendientes.")
-        else:
-            print("\nLista de tareas: ")
-            for i, tarea in enumerate(tareas, start=1):
-                print(f"{i}. {tarea}")
+        ver_tareas(tareas)
+        
     elif opcion == "2":
-        nueva_tarea = input("Escribe la nueva tarea: ")
-        if nueva_tarea.lower() in [t.lower() for t in tareas]:
-            print ("Esa tarea ya existe.")
-        else:
-            tareas.append(nueva_tarea)
-            guardar_tarea(nueva_tarea)
-            print("Tarea agregada correctamente.")
+        agregar_tarea(tareas)
+        
     elif opcion == "3":
         print("Saliendo del programa...")
         break
