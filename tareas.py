75fe8b548c6358e79cc3a9cd8a0fe1a08f95dc78
Manuel Varela <manuel.imt@hotmail.com>
2026-03-10 12:56:40 -0600

    Validación para evitar tareas duplicadas

---
+++ tareas.py
@@ -0,0 +1,38 @@
+def mostrar_menu():
+    print("\n--- GESTOR DE TAREAS ---")
+    print("1. Ver tareas")
+    print("2. Agregar tarea")
+    print("3. Salir")
+
+tareas = []
+try:
+    with open("tareas.txt", "r") as archivo:
+        for linea in archivo:
+            tareas.append(linea.strip())
+except FileNotFoundError:
+    pass
+while True:
+    mostrar_menu()
+    opcion = input("Selecciona una opcion: ")
+
+    if opcion == "1":
+        if len(tareas) == 0:
+            print("No hay tareas pendientes.")
+        else:
+            print("\nLista de tareas: ")
+            for i, tarea in enumerate(tareas, start=1):
+                print(f"{i}. {tarea}")
+    elif opcion == "2":
+        nueva_tarea = input("Escribe la nueva tarea: ")
+        if nueva_tarea in tareas:
+            print ("Esa tarea ya existe.")
+        else:
+            tareas.append(nueva_tarea)
+            with open("tareas.txt", "a") as archivo:
+                archivo.write(nueva_tarea + "\n")
+                print("Tarea agregada correctamente.")
+    elif opcion == "3":
+        print("Saliendo del programa...")
+        break
+    else:
+        print("\n Opción inválida.")
